b979a551a5efff29c07e9ed7ad35259455ef4560
DaBaCheMao <102657266+DaBaCheMao@users.noreply.github.com>
2022-05-01 01:04:31 +0800

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Configuration模块，用于读取配置文件(xml形式);一些常量配置，例如数据库连接字可以配置在Configuration中
+ */
+using System;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApplication1
+{
+    class ConfigurationProgram
+    {
+        public static void Test()
+        {
+            ReadAllSettings();
+            ReadSetting("Setting1");
+            ReadSetting("NotValid");
+
+            //AddUpdateAppSettings("NewSetting", "May 7, 2014");
+            //AddUpdateAppSettings("Setting1", "May 8, 2014");
+            //ReadAllSettings();
+
+            ReadProducts();
+        }
+
+        static void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                Console.WriteLine(result);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+        static void ReadProducts()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["WingtipToys"].ConnectionString;
+            string queryString = "SELECT Id, ProductName FROM dbo.Products;";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var command = new MySqlCommand(queryString, connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0}, {1}", reader[0], reader[1]));
+                    }
+                }
+
+            }
+        }
+    }
+}
